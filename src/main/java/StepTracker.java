5763e28b8b54698e3bdb4fd611538a6c98002690
Jalau3 <143014960+Jalau3@users.noreply.github.com>
2024-04-22 10:01:13 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 public class StepTracker
 {
  /* to be implemented here */
- private int minSteps; activeDays; totalSteps;
+ private int minSteps, activeDays, totalSteps;
  StepTracker(int n){
    minSteps = n;
    activeDays = 0;
